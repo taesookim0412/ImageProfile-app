16cfa0e04050218d0978d05e54afe0c54e21e1b7
taesookim0412 <taesoo.kim0412@gmail.com>
2021-10-17 00:43:41 -0700

    10162021 f-e intact, profile, close to Images micro (TF Java, or TF .NET) ? --logic fix

--- server/configs/Utilities/ValidationUtils.ts
+++ server/configs/Utilities/ValidationUtils.ts
@@ -1,3 +1,3 @@
 export function validateUsernameAndPassword(username: string, password: string){
-    return ((username === undefined || password == undefined) ||  (username.length > 4 && username.length < 12 && password.length > 4 && password.length < 20));
+    return (username !== undefined && password !== undefined &&  username.length > 4 && username.length < 20 && password.length > 4 && password.length < 20);
 }
