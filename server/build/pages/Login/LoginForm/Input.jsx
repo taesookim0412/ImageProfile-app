c039a2f5072dcd3a8b390b146e1aa3401cba8b6a
taesookim0412 <taesoo.kim0412@gmail.com>
2021-10-16 20:52:16 -0700

    10162021 cleanup main branch

--- server/build/pages/Login/LoginForm/Input.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Input(props) {
-    function handleChange(setState, newString) {
-        setState(newString);
-    }
-    return (<input type={props.type} name={props.name} value={props.value}/>);
-}
-exports.default = Input;
